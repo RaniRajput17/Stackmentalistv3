7d2ac665fd7542d61655b9fe7b942b61356dca80
Rani Rajput <108329615+RaniRajput17@users.noreply.github.com>
2024-05-24 12:39:28 +0530

    Stackmentalistv3

---
+++ src/pages/Team.js
@@ -0,0 +1,88 @@
+// import vishal from "../../assets/AligneLogos/vishal.jpeg";
+import vishal from "../../public/team/vishal.jpeg";
+import shashank from "../../public/team/shashank.jpg";
+import sanket from "../../public/team/sanket.jpg";
+import RABINASHARMA from "../../public/team/RABINASHARMA.jpg";
+import NEHA from "../../public/team/NEHA.jpg";
+import Nilesh from "../../public/team/Nilesh.jpg";
+import Vikrant from "../../public/team/Vikrant.jpg";
+import Vishal_tiwari from "../../public/team/VT2.png";
+import Mishamma from "../../public/team/ML.png";
+import Nishant from "../../public/team/NS.png";
+import Kaustubh from "../../public/team/KS.png";
+import Himanshu from "../../public/team/HT.png";
+
+const Teams = {
+  founder: [
+    {
+      name: "Rabina Sharma",
+      img: RABINASHARMA,
+      designation: "CMO",
+    },
+    {
+      name: "Vishal Kumar Tiwari",
+      img: Vishal_tiwari,
+      designation: "CFO",
+    },
+    {
+      name: "Vishal Sharma",
+      img: vishal,
+      designation: "CTO",
+    },
+  ],
+  Advisory_board: [
+    {
+      name: "Neha Naz",
+      img: NEHA,
+      designation: "Legal Consultant",
+    },
+    {
+        name: "Nishant Sharma",
+        img: Nishant,
+        designation: "Legal Consultant",
+      },
+  ],
+  team: [
+    {
+      name: "Nilesh Chandravanshi",
+      img: Nilesh,
+      designation: "Architect",
+    },
+    {
+      name: "Mishamma Sanyok Limbu",
+      img: Mishamma,
+      designation: "HR Head",
+    },
+    // {
+    //   name: "Nishant Sharma",
+    //   img: Nishant,
+    //   designation: "UI & Design Head",
+    // },
+    {
+      name: " Sanket Jain",
+      img: sanket,
+      designation: "Engineering Head",
+    },
+    {
+      name: "Kaustubh Singh",
+      img: Kaustubh,
+      designation: "Cloud & DevOps Head",
+    },
+    {
+      name: "Vikrant Giri",
+      img: Vikrant,
+      designation: "Data Science Head",
+    },
+    {
+      name: "Shashank Gupta",
+      img: shashank,
+      designation: "QA & UX Head",
+    },
+    {
+      name: "Himanshu Tripathi",
+      img: Himanshu,
+      designation: "Cyber Security Head",
+    },
+  ],
+};
+export default Teams;
